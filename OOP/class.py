48351118ce68230fc05c7005cd8ccfed115c8f2d
Bipin47 <bipindawadi92@gmail.com>
2026-09-12 11:58:04 +0545

    Class

--- OOP/class.py
+++ OOP/class.py
@@ -1,10 +1,25 @@
 class Student:
-    def __init__(self, name, age):
+    def __init__(self,name,mark):
         self.name = name
-        self.age = age
-        print("Name is "+ str(self.name) + " & Age is " + str(self.age))
+        self.mark = mark
+    
+
+    def cal_average(self):
+        sum = 0
+        for i in self.mark:
+            sum += i
+        return sum/len(self.mark)
 
 
-name1 = input("Enter name: ")
-age1 = int(input("Enter age: "))
-s1 = Student(name1, age1)
+name1= input("Enter the name of student: ")
+range_mark = int(input("Enter the number of subjects: "));
+
+list_1 = []
+for i in range(range_mark):
+    mark = int(input("Enter the marks of student: "))
+    list_1.append(mark)
+s1 = Student(name1,list_1)
+average = s1.cal_average()
+print(f"The average mark of {s1.name} is {average}")
+
+
