74e6ac8d6e78a3b32e01b5d2d1214178d4875330
Bipin47 <bipindawadi92@gmail.com>
2026-09-12 11:34:57 +0545

    OOP

---
+++ OOP/Constructor.py
@@ -0,0 +1,10 @@
+#constructor is _init_
+class Student:
+    name = 'bipin'
+    def __init__(self):
+        
+        print("This is a constructor")
+
+s1 = Student()
+
+
--- OOP/class.py
+++ OOP/class.py
@@ -1,1 +1,10 @@
-print("HEllo world");
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print("Name is "+ str(self.name) + " & Age is " + str(self.age))
+
+
+name1 = input("Enter name: ")
+age1 = int(input("Enter age: "))
+s1 = Student(name1, age1)
